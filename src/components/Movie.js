5c0befd11d7cdfded2ed008f4d9b439369fe28c4
Ansah1234 <ansahpeter123@yaoo.com>
2020-09-13 12:40:35 +0800

    SVG styling

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,7 +8,6 @@ const Movie = ({path}) => {
         <div  className='movie-poster-box'>
              <img src={path} alt='movie posters' className='movie-poster'></img>
              <div className='movie-poster__filter'>
-
              </div>
         </div>
        
