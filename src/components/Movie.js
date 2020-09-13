8ce6c704db35fd3ab30500c1470fd725802777fc
Ansah1234 <ansahpeter123@yaoo.com>
2020-09-13 12:03:05 +0800

    Light Green hover Effects Done

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,8 +2,16 @@ import React from 'react'
 import '../movie.css'
 
 const Movie = ({path}) => {
+
+
     return (
-        <img src={path} alt='movie posters' className='movie-poster'></img>
+        <div  className='movie-poster-box'>
+             <img src={path} alt='movie posters' className='movie-poster'></img>
+             <div className='movie-poster__filter'>
+
+             </div>
+        </div>
+       
     )
 }
 
